ae8c1f5f44dd0de53f36457a4992a30ea40732bb
0xceba <0xceba@protonmail.com>
2020-11-29 14:46:59 +0100

    revamp --max argument

--- README.md
+++ README.md
@@ -1,36 +1,39 @@
 # Sample Files Generator
 
-Generate sample files of type `exe`, `mp3`, `pdf`, `txt`, and `zip`. These sample files can be useful for software testing such as testing ransomware.
+Generate sample files of type `png`, `exe`, `mp3`, `pdf`, `txt`, and `zip`. These sample files can be useful for software testing such as when testing ransomware.
+
+### Dependencies
+
+* Requires the `/usr/share/dict/words` file which is provided by the `wordlist` package.
 
 ### Usage
 
 ```
-./sample_files_generator.py --help
-usage: sample_files_generator.py [-h] [-d] [--max {128,256,512,1024,2048,4096,8192,16384,32768,65536}] destination number_of_files
+./sample_files_generator.py -h
+usage: sample_files_generator.py [-h] [-d] [-m MAX] destination number_of_files
 
-Generate test files.
+Generate sample files.
 
 positional arguments:
-  destination           where to generate the test files.
-  number_of_files       how many test files to generate.
+  destination        where to generate the sample files.
+  number_of_files    how many test files to generate.
 
 optional arguments:
-  -h, --help            show this help message and exit
-  -d, --dir             create directories.
-  --max {128,256,512,1024,2048,4096,8192,16384,32768,65536}
-                        set the max size in bytes for each file.
+  -h, --help         show this help message and exit
+  -d, --dir          create directories.
+  -m MAX, --max MAX  set the max size in kilobytes for each file.
 ```
 
 ### Usage Examples
 
-Generate 2000 test files:
+Generate 2000 sample files:
 
 ```
-./sample_files_generator.py /home/gforeman/ransomware_sim/testfiles/ 2000
+./sample_files_generator.py /home/gforeman/ransomware_sim/samplefiles/ 2000
 ```
 
-Generate 2000 test files with directories and varied file sizes under 2048 bytes:
+Generate 2000 sample files with a random directory structure and file sizes under 5 MB:
 
 ```
-./sample_files_generator.py /home/gforeman/ransomware_sim/testfiles/ 2000 --max 2048 -d
+./sample_files_generator.py --max 5000 -d /home/gforeman/ransomware_sim/samplefiles/ 2000 
 ```
--- sample_files_generator.py
+++ sample_files_generator.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 '''
-* Requires the words file (provided by the wordlist package) located at /usr/share/dict/words
-* To add file extensions:
-	a) add additional extension to file_extensions list
-	b) add a template file for each new extension to ./template_files/ 
+* Requires the words file (provided by the wordlist package) to be located at /usr/share/dict/words
+* To add additional file extensions add a template file to ./template_files/ 
 '''
 
 import os, sys, argparse, random
@@ -31,6 +29,8 @@ def create_file(file_type, file_location, filename, max_size):
 		grow_file(destination_file, max_size)
 
 def grow_file(destination_file, max_size):
+	max_size = max_size * 1024
+
 	current_size = os.stat(destination_file).st_size
 	potential_bytes_to_add = max_size - current_size
 	num_of_bytes_to_add = random.randint(0,potential_bytes_to_add)
@@ -50,33 +50,44 @@ def open_dictionary(word_file):
 		print("Could not open/read file: " + word_file)
 		sys.exit()
 
+def get_file_extensions():
+	extension_list = []
+	template_directory = "./template_files/"
+
+	for template_filename in os.listdir(template_directory):
+		if template_filename.endswith("README.md"):
+			continue
+		else:
+			template_file_path = os.path.join(template_directory, template_filename)
+			extension = os.path.splitext(template_file_path)[1][1:]
+			extension_list.append(extension)
+
+	return extension_list
+
 def main(argv):
-	try:
-		parser = argparse.ArgumentParser(description='Generate test files.')
-		parser.add_argument('destination', help='where to generate the test files.', type=dir_path)
-		parser.add_argument('number_of_files', help='how many test files to generate.', type=int)
-		parser.add_argument('-d', '--dir', help='create directories.', action="store_true")
-		parser.add_argument('--max', help='set the max size in bytes for each file.', type=int, choices=[128,256,512,1024,2048,4096,8192,16384,32768,65536])
-		args = parser.parse_args()
+	parser = argparse.ArgumentParser(description='Generate sample files.')
+	parser.add_argument('destination', help='where to generate the sample files.', type=dir_path)
+	parser.add_argument('number_of_files', help='how many test files to generate.', type=int)
+	parser.add_argument('-d', '--dir', help='create directories.', action="store_true")
+	parser.add_argument('-m', '--max', help='set the max size in kilobytes for each file.', type=int)
+	args = parser.parse_args()
 
-		file_destination = args.destination
-		dir_list = [args.destination]
+	file_destination = args.destination
+	dir_list = [args.destination]
 
-		file_extensions = ['txt','pdf','mp3','zip','exe']
-		words = open_dictionary("/usr/share/dict/words")
+	file_extensions = get_file_extensions()
+	words = open_dictionary("/usr/share/dict/words")
 
-		for x in range(0, args.number_of_files):
+	for x in range(0, args.number_of_files):
+		filename = random.choice(words).replace("'", "").lower()
+		file_type = random.choice(file_extensions)
+
+		if args.dir and x % 13 == 0:
+			dir_list = create_directory(dir_list, filename)
 			filename = random.choice(words).replace("'", "").lower()
-			file_type = random.choice(file_extensions)
 
-			if args.dir and x % 13 == 0:
-				dir_list = create_directory(dir_list, filename)
-				filename = random.choice(words).replace("'", "").lower()
-
-			new_file = create_file(file_type, get_dir_destination(dir_list), filename, args.max)
-		print('Test files created successfully.')
-	except Exception as ex:
-		print('Exception: ' + ex)
+		new_file = create_file(file_type, get_dir_destination(dir_list), filename, args.max)
+	print('Test files created successfully.')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
